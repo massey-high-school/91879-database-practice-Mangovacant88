f87dd2ece9e5183e0a4813fe9eee9a90cda03f61
Mangovacant88 <60583355+Mangovacant88@users.noreply.github.com>
2020-08-07 10:55:50 +1200

    :)

---
+++ bottombit.php
@@ -0,0 +1,7 @@
+        <div class="box footer">
+            CC Jordan Charlton 2020
+        </div>    <!-- / footer -->
+    
+    </div>  <!-- / wrapper  -->
+    
+</body>
---
+++ topbit.php
@@ -0,0 +1,64 @@
+<!DOCTYPE HTML>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Book Review Database">
+    <meta name="keywords" content="books, reading, fiction, non-fiction, genre, reviews">
+    <meta name="author" content="Jordan Charlton">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Book Review Database</title>
+    
+    <!-- Edit the link below / replace with your chosen google font -->
+    <link href="https://fonts.googleapis.com/css?family=Lato%7cUbuntu" rel="stylesheet"> 
+    
+    <!-- Edit the name of your style sheet - 'foo' is not a valid name!! -->
+    <link rel="stylesheet" href="css/bookstyle.css"> 
+    
+</head>
+    
+<body>
+    
+    <div class="wrapper">
+    
+
+        
+        <div class="box banner">
+            
+        <!-- logo image linking to home page goes here -->
+        <a href="index.php">
+            <div class="box logo"  title="Logo - Click here to go to the Home Page">
+            <img class="img-circle" src="images/book_logo.jpg" width="150" height="117" alt="generic logo" />
+            
+            </div>    <!-- / logo -->
+        </a>
+            
+            <h1>Orchid Reading</h1>
+        </div>    <!-- / banner -->
+
+        <!-- Navigation goes here.  Edit BOTH the file name and the link name -->
+        <!--
+        <div class="box nav">
+            <a href="index.html">Home</a> | 
+            <a href="page1.html">Page 1</a> | 
+            <a href="page2.html">Page 2</a> | 
+            <a href="page3.html">Page 3</a> | 
+            <a href="contact.html">Contact</a>
+        </div>    <!-- / nav -->        
+        
+        <div class="box side">
+            
+        <h2>Search | <a class="side" href="showall.php">Show All</a></h2>
+            
+        <p><i>Type part of the title / author name if desired</i></p>
+        
+        <hr />
+        
+        Title Search<br />
+        Author Search<br />
+        Genre Search<br />
+        Ration Search
+            
+        </div>      <!-- / side bar -->
